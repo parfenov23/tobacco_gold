4fc0766c25f4d69706544fc1500ded42f461588d
maks <prospu@qip.ru>
2016-04-11 02:57:56 +0500

    edit n chat

--- app/models/other_buy.rb
+++ app/models/other_buy.rb
@@ -2,7 +2,7 @@ class OtherBuy < ActiveRecord::Base
 
   def notify_buy
     message = "#{title} на #{price.to_i} рублей\nКасса: #{Sale.cash_box} рублей"
-    chat_id = "54"
+    chat_id = "53"
     auth_key = "e1cc23af2d7c44bcb851c1ae82152a0db5816c98f03218db5e2d8f27a75aeb6f14d98dbe2d75fc4eab7f4"
     Mechanize.new.get("https://api.vk.com/method/messages.send?chat_id=#{chat_id}&message=#{message}&access_token=#{auth_key}")
   end
