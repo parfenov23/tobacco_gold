6ab04c9ae52eb8d16d258edccc2e73ee25d62a13
maks <prospu@qip.ru>
2016-10-19 04:14:24 +0500

    fix

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class AdminController < ActionController::Base
     layout "admin"
     def index
-  		redirect_to "/stock" if !current_user.present?
+  		redirect_to "/stock" if !current_user.admin
   	end
   end
 end
