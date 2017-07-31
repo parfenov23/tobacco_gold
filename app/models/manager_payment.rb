f6bca829f81b1c2f89c9e0cc3be5a3db727540e5
maks <prospu@qip.ru>
2017-08-01 00:19:53 +0500

    fix mp

--- app/models/manager_payment.rb
+++ app/models/manager_payment.rb
@@ -3,7 +3,7 @@ class ManagerPayment < ActiveRecord::Base
 
   def self.curr_month_price
     start_day = (Time.now - Time.new.day.day + 1.day).beginning_of_day
-    all_others = where(["created_at > ?", start_day])
+    all_others = where(["created_at > ?", start_day]).where(payment: true)
     all_others.sum(:price)
   end
 end
