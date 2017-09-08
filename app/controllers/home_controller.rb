e6ceab51b43ccb09ae15a6b822dee8adf45e6675
maks <prospu@qip.ru>
2017-09-08 19:58:42 +0500

    add top item

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ActionController::Base
       Product.find(params[:product_id]).product_items
     else
       arr_ids = ProductItem.where(["count > ? ", 0]).map{|pi| {id: pi.id, count: pi.sale_items.count} if pi.count > 0}.compact.sort_by { |hsh| hsh[:count] }.reverse.first(20).map{|pi| pi[:id]}
-      ProductItem.where(ids: arr_ids)
+      ProductItem.where(id: arr_ids)
     end
     if params[:price].present?
       ids = @items.map do |item| 
