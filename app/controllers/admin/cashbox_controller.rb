8316b6af370d94204e77b9551c7532028ed05013
parfenov <parfenov407@gmail.com>
2017-11-07 22:19:58 +0500

    add stat api cash v1.3

--- app/controllers/admin/cashbox_controller.rb
+++ app/controllers/admin/cashbox_controller.rb
@@ -62,13 +62,21 @@ module Admin
     def api
       cashbox = Cashbox.first
       all_sales = Sale.where(magazine_id: cashbox.magazine_id)
+      curr_month_manager_pay = ManagerPayment.where(magazine_id: cashbox.magazine_id).curr_month_price
+      curr_month_other_down_price = OtherBuy.where(magazine_id: cashbox.magazine_id).curr_month_price(false).to_i
+      curr_month_other_up_price = OtherBuy.where(magazine_id: cashbox.magazine_id).curr_month_price(true).to_i
+
+      curr_profit = all_sales.current_month_profit - curr_month_manager_pay - curr_month_other_down_price + curr_month_other_up_price
       render json: {cashbox:
-        {
-          cash: cashbox.cash, 
-          visa: cashbox.visa, 
-          today_sale: all_sales.current_day.sum(:price),
-          profit_today: all_sales.current_day_profit
-        }
+        [
+          {name: "cash", val: cashbox.cash}, 
+          {name: "visa", val: cashbox.visa}, 
+          {name: "saleToday", val: all_sales.current_day.sum(:price)},
+          {name: "profitToday", val: all_sales.current_day_profit},
+          {name: "saleMonth", val: all_sales.curr_month_price},
+          {name: "profit", val: all_sales.current_month_profit},
+          {name: "leftProfit", val: curr_profit}
+        ]
       }
     end
 
