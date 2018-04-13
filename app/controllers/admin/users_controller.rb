3bb41759b58b5191ef6617d642b7e8636aa93e2f
parfenov <parfenov407@gmail.com>
2018-04-14 01:24:17 +0500

    fix bug create users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,11 @@
 module Admin
   class UsersController < CommonController
 
+    def create
+      model.create(params_model.merge(password: params[:users][:password]))
+      params[:typeAction] == "json" ? render_json_success : redirect_to_index
+    end
+
     def index
       @models = model.where(magazine_id: current_company.magazines.ids).where(role: ["admin", "manager"])
     end
