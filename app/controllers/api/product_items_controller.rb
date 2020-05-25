f4441e8e627f368f79aeda2c910af3657e69476e
parfenov <parfenov407@gmail.com>
2020-05-25 13:28:14 +0500

    2.0.4 fix search

--- app/controllers/api/product_items_controller.rb
+++ app/controllers/api/product_items_controller.rb
@@ -18,12 +18,6 @@ module Api
       render json: ProductItem.find(params[:id]).transfer_to_json(params[:api_key])
     end
 
-    def search
-      search_items = ProductItem.search_title_or_barcode(current_company.id, params[:search])
-      search_items = search_items.all_present(current_magazine.id)
-      render json: search_items.transfer_to_json
-    end
-
     def get_search
       product_item = ProductItem.where(barcode: params[:barcode], product_id: Product.where(company_id: current_company.id).ids).last
       render text: "<p>" + (product_item.present? ? product_item.get_description : "Нет описания") + "<p>"
@@ -73,7 +67,7 @@ module Api
     end
 
     def search
-      render json: current_company.products.product_items.full_text_search(params[:search]).transfer_to_json
+      render json: current_company.products.product_items.full_text_search(params[:search]).all_present(current_magazine.id).transfer_to_json
     end
 
     def get_img_url
